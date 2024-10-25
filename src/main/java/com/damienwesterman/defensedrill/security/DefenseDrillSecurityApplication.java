e0f259cb43f83ae1460c920c733218413d7ecaa6
Damien Westerman <damien.westerman@snhu.edu>
2024-10-24 19:31:08 -0600

    Skeleton for spring

---
+++ src/main/java/com/damienwesterman/defensedrill/security/DefenseDrillSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.damienwesterman.defensedrill.security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefenseDrillSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DefenseDrillSecurityApplication.class, args);
+	}
+
+}
